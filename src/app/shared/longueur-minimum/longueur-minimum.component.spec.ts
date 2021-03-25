f005165420b3a5cc45e5f386d6d2987efd8e5163
AndyGiraud <1796067@cmontmorency.qc.ca>
2021-03-25 13:42:22 -0400

    FAIT: < TP8 & TP9 >

---
+++ src/app/shared/longueur-minimum/longueur-minimum.component.spec.ts
@@ -0,0 +1,98 @@
+import { AbstractControl } from "@angular/forms";
+
+import { ZonesValidator } from "./longueur-minimum.component";
+
+describe('longueur Zones Validator', () => {
+
+    it('#7 | une chaîne avec 10 espaces est invalide', () =>{
+        
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "          "};
+        
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result['nbreCaracteresInsuffisants']).toBe(true);
+
+    });
+
+    it('#8 | Une phrase avec des mots est valide', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "Vive angular"};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result).toBeNull();
+
+    });
+
+    it('#9 | Une phrase avec 3 espaces, des mots et ensuite 3 espaces est valide', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "   je le veux   "};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result).toBeNull();
+
+    });
+
+    it('#10 | Une phrase avec 1 espace et 2 caractères est invalide.', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: " xx"};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result['nbreCaracteresInsuffisants']).toBe(true);
+
+    });
+
+    it('#11 | Une phrase avec 2 espace et 1 caractères est invalide.', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "  x"};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result['nbreCaracteresInsuffisants']).toBe(true);
+
+    });
+
+    it('#12 | Une phrase avec 3 espaces et 3 caractères est valide', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "   xxx"};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result).toBeNull();
+
+    });
+
+    it('#13 | Une phrase avec 3 espaces et 3 caractères est valide', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: "     xxxxx     "};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result).toBeNull();
+
+    });
+
+    it('#14 | Une chaîne nulle est invalide', () =>{
+        //Préparer une variable pour manipuler le validateur
+        let validator = ZonesValidator.longueurMinimum();
+        let valeurControle  = {value: null};
+        //Faire l'appel du validateur 
+        let result=validator(valeurControle as AbstractControl);
+        //Comparer le resultat OBTENUE avec le résutat PRÉVU
+        expect(result['nbreCaracteresInsuffisants']).toBe(true);
+
+    });
+
+});
---
+++ src/app/shared/longueur-minimum/longueur-minimum.component.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+
+export class ZonesValidator{
+    static longueurMinimum(): ValidatorFn{
+        //Sous ANGULAR dans les validateurs pour indiquer un succès retourner NULL autrement retourner une clé valeur JSON
+        return(valeurControle:AbstractControl): {[key:string]: boolean} | null => {
+            
+
+            
+            if( valeurControle.value != null && valeurControle.value.trim().length >= 3){
+
+                return null;
+            }
+
+            return {'nbreCaracteresInsuffisants' : true};
+        };
+    }
+}
